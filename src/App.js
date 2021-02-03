7b3102fc9e93f918143144cdb4e78d5b1020942f
Jackie Tian <>
2021-02-04 10:50:55 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Main />
+		</>
+	);
+}
+
+export default App;
